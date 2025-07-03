724548fac17a6655d1976bb18ca89ee1a01d5e50
pt-br <lucastavares.developer@gmail.com>
2025-07-03 15:37:10 -0300

    chore: setup proxy, eslint, prettier and import alias

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,18 @@ export default defineConfig(() => {
     },
     server: {
       port: 3000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001/api',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
     plugins: [react()],
   };
